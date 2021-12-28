6b998608a5ccac603c3009b6409ea1ab100b6ee4
ario baskoro <ariobaskoro@arios-MacBook-Pro-2.local>
2021-12-28 21:09:44 +0700

    add query string

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// //dengan menggunakan path untuk first name dan last name saya persiapkan untuk menggunakan query dibawah karena masih belum bisa menggunakan hanya  1 app.get untuk /path/otherpath saya menggunakan path dengan membuat 2 app.get yg satu untuk /name dan satu lg untuk /name/first atau last.
+// //dengan menggunakan path untuk first name dan last name saya persiapkan untuk menggunakan query dibawah karena masih belum bisa menggunakan hanya  1 app.get untuk yang versi ini (/path/otherpath) saya menggunakan path dengan membuat 2 app.get yg satu untuk /name dan satu lg untuk /name/first atau last.
 const express = require('express');
 const app = express();
 
@@ -19,3 +19,24 @@ app.get('/name/:namePath', (req, res) => {
     }
 })
 app.listen(3000, () => console.log('on port 3000 bro'));
+
+// //dengan menggunakan query untuk first name dan last name
+// //dapat dipanggil menggunakan query sebagai berikut http://localhost:3000/name?q=last or http://localhost:3000/name?q=first or for full name http://localhost:3000/name
+// const express = require('express');
+// const app = express();
+
+// let first = 'Ario';
+// let last = 'Baskoro';
+
+// app.get('/name', (req, res) => {
+//     const { q } = req.query;
+//     if (!q) {
+//         console.log(first, last);
+//     }
+//     if (q === 'first') {
+//         console.log(first)
+//     } else if (q === 'last') {
+//         console.log(last);
+//     }
+// })
+// app.listen(3000, () => console.log('on port 3000 coy'));
